c9a0758a35eaf7840a0a714a882c42e03c1a8dc8
Christophe Blaess <christophe.blaess@logilin.fr>
2021-12-17 07:49:47 +0100

    [DEV] Add day 17, part 2.

---
+++ 2021/17.2/code.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define BUFFER_SIZE 4096
+
+int target_xmin, target_xmax, target_ymin, target_ymax;
+
+int nb_success = 0;
+
+void fire(int vx, int vy);
+
+#define MIN_VX 1
+#define MAX_VX 200
+#define MIN_VY -200
+#define MAX_VY 200
+
+int main(int argc, char *argv[])
+{
+	char buffer[BUFFER_SIZE];
+
+	if (fgets(buffer, BUFFER_SIZE - 1, stdin) == NULL) {
+		fprintf(stderr, "No input data\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (sscanf(buffer, "target area: x=%d..%d, y=%d..%d", &target_xmin, &target_xmax, &target_ymin, &target_ymax) != 4) {
+		fprintf(stderr, "unable to parse line: %s\n", buffer);
+		exit(EXIT_FAILURE);
+	}
+
+	for(int vx = MIN_VX; vx < MAX_VX; vx++) {
+		for (int vy = MIN_VY; vy < MAX_VY; vy++) {
+			fire(vx, vy);
+		}
+	}
+
+	fprintf(stdout, "Nb success = %d\n", nb_success);
+
+	return 0;
+}
+
+void fire(int vx, int vy)
+{
+	int x=0;
+	int y=0;
+
+	for (;;) {
+		if ((x >= target_xmin) && (x <= target_xmax) && (y >= target_ymin) && (y <= target_ymax))
+			break;
+		if ((y < target_ymin))
+			return;
+		x += vx;
+		y += vy;
+		if (vx > 0) vx --;
+		if (vx < 0) vx --;
+		vy --;
+	}
+	nb_success ++;
+}
+
+
