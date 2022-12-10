de8f9161e3f99ea99ceac36a9e596ba201fe63c8
Christophe Blaess <christophe.blaess@logilin.fr>
2022-12-10 10:25:46 +0100

    [2022] Add day 10, part 1.

---
+++ 2022/aoc-2022-12-10-1/src/main.rs
@@ -0,0 +1,45 @@
+
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+
+fn main() -> io::Result<()> {
+
+	let reader = BufReader::new(io::stdin());
+
+	let mut cycles:i32 = 0;
+	let mut x:i32 = 1;
+
+	let mut sum: i32 = 0;
+
+	for line in reader.lines() {
+		let strline = line.unwrap();
+
+		if strline.starts_with("noop") {
+			sum += inc_cycles(&mut cycles, x);
+			continue;
+		}
+
+		if strline.starts_with("addx ") {
+			let value: i32 = strline[5..].parse::<i32>().unwrap();
+			sum += inc_cycles(&mut cycles, x);
+			sum += inc_cycles(&mut cycles, x);
+			x += value;
+		}
+	}
+
+	println!("Sum = {}", sum);
+
+	Ok(())
+}
+
+
+fn inc_cycles(cycles: &mut i32, x: i32) -> i32 {
+	*cycles += 1;
+	if (*cycles % 40) == 20 {
+		// println!("cycle {}, strength = {}", *cycles, *cycles * x);
+		return *cycles * x;
+	}
+	return 0;
+}
